dd60f2e17da0b103507a08b18f80a0df61194db1
Valdir Cezar <valdircezar312@gmail.com>
2021-06-14 17:27:07 -0300

    ajuste deploy

--- src/main/java/com/valdir/senaivacinas/configurations/SecurityConfig.java
+++ src/main/java/com/valdir/senaivacinas/configurations/SecurityConfig.java
@@ -28,54 +28,49 @@ import com.valdir.senaivacinas.security.JWTUtil;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
-    private JWTUtil jwtUtil;
-	
-	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**"};
-	private static final String[] PUBLIC_MATCHERS_POST = { "/usuarios/**", "/login/**"};
+	private JWTUtil jwtUtil;
+
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+	private static final String[] PUBLIC_MATCHERS_POST = { "/usuarios/**", "/login/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.cors();
 		http.csrf().disable();
-		
+
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll()
-		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-		.anyRequest().authenticated();
-		
+				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll().anyRequest().authenticated();
+
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        
+
 	}
-	
+
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html",
-                "/webjars/**");
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+				"/configuration/security", "/swagger-ui.html", "/webjars/**");
 	}
-	
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
@@ -84,10 +79,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 }
